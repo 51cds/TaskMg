d75c4f340886e072a00a50c47b1cf6d8758feef8
51cds <1970221702@qq.com>
2017-11-16 13:17:25 +0800

    项目搭建

---
+++ setvlet/src/main/java/com/lzz/taskmg/TestController.java
@@ -0,0 +1,15 @@
+package com.lzz.taskmg;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @描述：${DESCRIPTION}
+ * @创建者：Zesheng.lin
+ * @日期：2017-11-16 13:09
+ * @修改者：Zesheng.lin
+ * @修改日期：2017-11-16 13:09
+ * @版本：v1.0
+ **/
+@Controller
+public class TestController {
+}
